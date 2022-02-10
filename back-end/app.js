fb85c84c044b0a2d9890550bbbd1b06f7d18e7cd
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2022-02-10 18:03:23 -0500

    backend semi-completed

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,24 @@
 // DEPENDENCIES
-
+const app = express();
+const cors = require("cors");
 const express = require("express");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
 // ROUTES
+app.get("/", (request, response) => {
+    response.send("Welcome to the Snack-A-Log!");
+});
+
+//Star(*) matches anything we haven't matched yet.
+app.get("*", (request, response) => {
+response.status(404).json({error: "Page not found"});
+});
 
 // EXPORT
 module.exports = app;

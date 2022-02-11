ff4ec2b1273b8b245dc564546da9ce5ee9ea12da
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2022-02-11 16:08:52 -0500

    confirm health tests passing

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -2,10 +2,18 @@ const snacksController = require('./controllers/snackController');
 
 
 const confirmHealth = (snack) => {
+    if(isNaN(snack.protein) || isNaN(snack.added_sugar) || isNaN(snack.fiber)){
+        return null;
+    }
+    
     if(snack.protein >= 5 && snack.added_sugar < 5 || snack.fiber >= 5 && snack.added_sugar < 5){
         return true;
     }
     return false;
+    
 };
 
+
+
 module.exports = confirmHealth;
+

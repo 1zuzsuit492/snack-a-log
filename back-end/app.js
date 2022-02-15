20148bb7f6da46acd2c75945576e34ebc85767a2
Bryant Mercedes <87352248+BryantMercedes212@users.noreply.github.com>
2022-02-15 12:56:52 -0500

    Revert "Bryant frontend"

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,12 @@
 // DEPENDENCIES
-const snacksController = require("./controllers/snackController");
+const snacksController = require('./controllers/snackController');
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors')
 const PORT = process.env.PORT || 3003;
 
 // CONFIGURATION
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
 // MIDDLEWARE
 
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom"
 // import NavBar from "./Components/Common/NavBar";
 import AllSnacks from "./Components/AllSnacks";
 // import SnackDetail from "./Components/SnackDetail";
 // import NewSnack from "./Components/NewSnack";
-import SnackEdit from "./Components/SnackEdit";
-import Nav from "./Components/Nav";
+import SnackEdit from "./Components/SnackEdit"
+
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Routes>
-        <Route exact path="/" element={<h2>Hello, world!</h2>} />
-        <Route path="/snacks" element={<AllSnacks />} />
-        {/* <Route path="/snacks/:id" element={<SnackDetail/>}/> */}
-        {/* <Route path="/snacks/new" element={<NewSnack/>}/> */}
-        <Route path="/:id/edit" element={<SnackEdit />} />
-      </Routes>
+        {/* <NavBar/> */}
+        <Routes>
+          <Route exact path="/" element={<h2>Hello, world!</h2>}/>
+          <Route path="/snacks" element={<AllSnacks/>}/>
+          {/* <Route path="/snacks/:id" element={<SnackDetail/>}/> */}
+          {/* <Route path="/snacks/new" element={<NewSnack/>}/> */}
+          <Route path ="/:id/edit" element={<SnackEdit />} />
+        </Routes>
     </div>
   );
 }

79900e1d13f26ae25cd4a05fae76c4bdbcb88f27
Melissa Lopez <87431063+1zuzsuit492@users.noreply.github.com>
2022-02-15 11:22:32 -0500

    Revert "added allsnacks, modified heart health and snack edit"

--- front-end/src/Components/HeartHealth.js
+++ front-end/src/Components/HeartHealth.js
@@ -2,12 +2,11 @@ import heartSolid from "../assets/heart-solid.png";
 import heartOutline from "../assets/heart-regular.png";
 
 function HeartHealth({ snackHealth }) {
-  if(snackHealth){
-  return <img className="heartSolid" src={heartSolid} alt={"healthy food"}/>;
-  }
-  return <img className="heartOutline" src={heartOutline} alt={"unhealthy food"}/>;
+  return (
+    <>
+      <p>????</p>
+    </>
+  );
 }
 
 export default HeartHealth;
-
-//snackHealth is a Boolean value (true or false) that is being passed down as a prop from a parent component
